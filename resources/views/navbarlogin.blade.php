444b1f9c35e4370b2dd1f0389a5b739b1cfc1fe0
Juan Felipe Lara <juanlara17@hotmail.com>
2019-10-20 11:22:56 -0500

    Se arreglo el finalizar sesion

--- resources/views/navbarlogin.blade.php
+++ resources/views/navbarlogin.blade.php
@@ -1,10 +1,17 @@
 @if (Auth::check())
-    <li class="dropdown">
-        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+    <li class="nav-item dropdown">
+        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false" aria-haspopup="true" v-pre>
             <i class="fa fa-user"></i> {{ Auth::user()->user }} <span class="caret"></span>
         </a>
-        <ul class="dropdown-menu">
-            <li><a href="{{ route('logout') }}">Finalizar Sesion</a></li>
+        <ul class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+            <a href="{{ route('logout') }}" class="dropdown-item"
+               onclick="event.preventDefault();
+                    document.getElementById('logout-form').submit();">
+                {{ __('Finalizar Sesion') }}
+            </a>
+            <form action="{{ route('logout') }}" id="logout-form" method="post" style="display: none">
+                @csrf
+            </form>
         </ul>
     </li>
 @else
@@ -14,7 +21,7 @@
             <i class="fa fa-user"></i>
         </button>
         <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-            <a class="dropdown-item" href="#">Iniciar sesion</a>
+            <a class="dropdown-item" href="{{ route('home') }}">Iniciar sesion</a>
         </div>
     </div>
 </li>
